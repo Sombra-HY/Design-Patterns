54433a427aa350415efce422c24aa7657b3fbe09
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-12-18 13:32:11 -0300

    MonoState

---
+++ src/Creational/Singleton/MonoStage/MonoStage.java
@@ -0,0 +1,29 @@
+package Creational.Singleton.MonoStage;
+//"""
+//Monostate (ou Borg) - É uma variação do Singleton proposto
+//por Alex Martelli que tem a intenção de garantir que o
+//estado do objeto seja igual para todas as instâncias.
+//"""
+class Monostatege {
+    private static int valorCompartilhado;
+
+    public void setValor(int novoValor) {
+        valorCompartilhado = novoValor;
+    }
+
+    public int getValor() {
+        return valorCompartilhado;
+    }
+}
+
+// Uso da classe Monostate
+ class Main {
+    public static void main(String[] args) {
+        Monostatege instancia1 = new Monostatege();
+        Monostatege instancia2 = new Monostatege();
+
+        instancia1.setValor(10);
+
+        System.out.println(instancia2.getValor()); // Saída será 10
+    }
+}
